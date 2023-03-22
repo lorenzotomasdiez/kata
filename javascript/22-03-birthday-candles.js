cf4c84414872dfbe11ec4a390a0a65692fdca3c0
Lorenzo Tomas Diez <lorenzotomasdiez@Lorenzos-Mac-mini.local>
2023-03-22 06:54:21 -0300

    Daily Kata: 22-03

---
+++ javascript/22-03-birthday-candles.js
@@ -0,0 +1,54 @@
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+  // Write your code here
+  let max = candles.reduce((acc, el) => el > acc ? el : acc, 0);
+  let result = candles.reduce((acc,el) => el === max ? acc + 1 : acc, 0);
+  console.log(result);
+  return result;
+}
+
+
+birthdayCakeCandles([ 3, 2, 1, 3 ])
+
+/*
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+
+The maximum height candles are  units high. There are  of them, so return .
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+Input Format
+
+The first line contains a single integer, , the size of .
+The second line contains  space-separated integers, where each integer  describes the height of .
+
+Constraints
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+Candle heights are . The tallest candles are  units, and there are  of them.
+*/
